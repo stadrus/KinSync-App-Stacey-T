d8c41d5b089d33e6db86c02dc3394bf6a978b1e0
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-05-23 10:09:39 -0500

    M1 complete

---
+++ kinsync-app-St/src/component/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Content from "./Content"
+
+const Dashboard = () => {
+    return(
+        <div className="dashboard">
+            <Content />
+        </div>
+    )
+};
+
+export default Dashboard; 
