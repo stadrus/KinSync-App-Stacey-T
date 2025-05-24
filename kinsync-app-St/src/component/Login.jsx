76bcb278cf004244779a0faf3d781760632778ee
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-05-23 21:09:22 -0500

    localstorage practice

--- kinsync-app-St/src/component/Login.jsx
+++ kinsync-app-St/src/component/Login.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 const Login = () => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = setSate('');
+    const navigate = useNavigate();
+    
+   localStorage.getItem('isLoggedin', true);
+    
+    const handleSubmit = () => {
+        if (userName === 'name' && password === 'password'){
+            console.log("Login Successful");
+            navigate('/Dashboard');
+        } else{
+            console.log("Email or password is invalid");
+        }
+    };
+
+    const handleClick = () =>{
+        navigate('/Register')
+    };
+
     return (
-        <div className="login">
+        <div>
             <h1>Login</h1> 
-            <form className="login">
-                <label>Email</label>
-                <input type="email" placeholder="Enter Email"></input>
-                <label>Password</label>
-                <input type="password" placeholder="Enter Password"></input>
-            </form>   
+            <form onSubmit={handleSubmit}>
+                <input type="email" placeholder="Enter Email"/>
+                <input type="password" placeholder="Enter Password"/>
+                <button type="submit">Submit</button><br></br>
+                <button type="button" onClick={handleClick}>Register</button>
+            </form>
         </div>
     );
 };
--- kinsync-app-St/src/component/Register.jsx
+++ kinsync-app-St/src/component/Register.jsx
@@ -1,17 +1,43 @@
 import images from '../images/profile.svg'
+import { useNavigate } from 'react-router';
+import { useState } from 'react';
+
 
 const Register = () => {
+    
+    const [userData, setUserData] = useState({
+        id:0,
+        name:"",
+        email:"",
+        password:"",
+        correctPassword: "",
+    });
+
+    //include use state for each key//
+    //create onChange elements for each key e.targt.value setStateName(key)
+
+    localStorage.setItem('user', setUserData);
+
+    const navigate = useNavigate();
+   
+    
+    const handleSubmit = () => {
+        if (userData.password !== userData.correctPassword){
+            alert ("Passwords do not match");
+        } else {
+            navigate('/Login');
+        }
+    }
     return (
-        <div className="login">
+        <div>
             <h1>Register Today!</h1>
             <img className="blankpic" src={images} alt="blank user profile"></img>
-            <form className="login">
-                <label>Name</label>
-                    <input type="text" placeholder="Enter Name"></input>
-                <label>Email</label>
-                    <input type="email" placeholder="Enter Email"></input>
-                <label>Password</label>
-                    <input type="password" placeholder="Enter Password"></input>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Enter Name' value={userData.name}/><br></br>
+                <input type="email" placeholder='Enter Password' value={userData.email}/><br>
+                </br>
+                <input type="password" placeholder='Enter Password' value={userData.password}/><br></br>
+                <button type="submit">Submit</button>
             </form>   
         </div>
     );
